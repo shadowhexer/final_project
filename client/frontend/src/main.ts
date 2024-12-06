eb8ac8146fe9a4236463083f8d3aa9164a1272fc
shadowhexer <austinmanlangit@gmail.com>
2024-12-07 06:38:56 +0800

    Created

---
+++ client/frontend/src/main.ts
@@ -0,0 +1,31 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
+const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    }
+});
+
+const route = router.beforeEach((to, from, next) => {
+    document.title = to.meta.title as string || 'Page Not Found';
+    next();
+});
+
+app.use(route).use(vuetify).mount('#app')
---
+++ client/frontend/src/types/modules.d.ts
@@ -0,0 +1,37 @@
+// src/shims-vue.d.ts
+declare module '@jamescoyle/vue-icon';
+
+declare module '@mdi/js';
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+  
